7dfd0dba350dfe0f48a18143e72b7f3362b265da
Alexander Kochurov <the.solarstone@gmail.com>
2019-10-05 00:03:01 +0300

    basic unit tests

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,12 @@ FROM python
 ENV DJANGO_SETTINGS_MODULE=bond_db.settings.local
 ENV DJANGO_SECRET_KEY=0nvp)2o93(=7f$j5siuu9e$v-w^2_ho7*fpiitfyfuh(*1)v34
 
-COPY requirements.txt /apps-conf
-RUN pip3 install -r /apps-conf/requirements.txt
-
 RUN mkdir /apps-conf
 RUN mkdir /apps
 COPY /app /apps/bdb
 COPY config/bdb.ini /apps-conf
+COPY requirements.txt /apps-conf
+RUN pip3 install -r /apps-conf/requirements.txt
 
 EXPOSE 5000
 CMD bash /apps/bdb/scripts/init_uwsgi.sh
--- app/core/tests.py
+++ app/core/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Country, Currency, BondIssue, Comment
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def setUp(self):
+        Country.objects.create(abbr="GV", name="Genovia")
+        Currency.objects.create(abbr="SP", name="Genovian sepulka", country=Country.objects.get(name="Genovia"))
+
+    def test_create_country_currency(self):
+        """ Создание моделей Country и Currency """
+        genovia = Country.objects.get(name="Genovia")
+        sepulka = Currency.objects.get(name="Genovian sepulka")
+        self.assertEqual(genovia.abbr, "GV")
+        self.assertEqual(sepulka.abbr, "SP")
+
+# Добавить юниттесты для: 
+# - список выпусков и пагинации по ним
+# - индивидуальный выпуск
+# - логина как va, добавления комментария к некоторому выпуску, логаута
+# - несколько вариантов поиска по базе: пустой/конкретный/етц
+# - авторизация как админ и загрузка новых выпусков (малого/большого объема)
+
+# - загрузка статьи в блог, в новости
+# - список статей, новостей
+# - индивидуальная статья, новость
+
